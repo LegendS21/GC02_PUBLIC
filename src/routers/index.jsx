652e64a624fd736b0c2f08aa9065695e67187a6f
LegendS21 <69729678+LegendS21@users.noreply.github.com>
2024-09-28 20:02:44 +0700

    Add files via upload

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -8,65 +8,13 @@ const url = "https://h8-phase2-gc.vercel.app";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login url={url} />,
-    loader: () => {
-      if (localStorage.token) {
-        Toastify({
-          text: "You already logged in",
-          duration: 2000,
-          newWindow: true,
-          close: true,
-          gravity: "bottom",
-          position: "right",
-          stopOnFocus: true,
-          style: {
-            background: "#EF4C54",
-            color: "#17202A",
-            boxShadow: "0 5px 10px black",
-            fontWeight: "bold",
-          },
-        }).showToast();
-        return redirect("/");
-      }
-      return null;
-    },
+    path: "/",
+    element: <HomePage url={url} />,
   },
-  {
-    element: <BaseLayout />,
-    loader: () => {
-      if (!localStorage.token) {
-        Toastify({
-          text: "Please log in first",
-          duration: 2000,
-          newWindow: true,
-          close: true,
-          gravity: "bottom",
-          position: "right",
-          stopOnFocus: true,
-          style: {
-            background: "#EF4C54",
-            color: "#17202A",
-            boxShadow: "0 5px 10px black",
-            fontWeight: "bold",
-          },
-        }).showToast();
-        return redirect("/login");
-      }
-      return null;
-    },
-    children: [
-      {
-        path: "/",
-        element: <HomePage url={url} />,
-      },
 
-      {
-        path: "/detail/:id",
-        element: <Detail url={url} />,
-      },
-      
-    ],
+  {
+    path: "/detail/:id",
+    element: <Detail url={url} />,
   },
 ]);
 export default router;
