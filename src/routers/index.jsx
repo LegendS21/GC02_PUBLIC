a7b7fb7fc76248cf5b5f7b228f6d97008c94ed53
LegendS21 <69729678+LegendS21@users.noreply.github.com>
2024-09-28 09:47:20 +0700

    Add files via upload

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -3,6 +3,7 @@ import Login from "../views/Login";
 import HomePage from "../views/Home";
 import BaseLayout from "../views/BaseLayout";
 import Toastify from "toastify-js";
+import Detail from "../views/PostDetail";
 const url = "https://h8-phase2-gc.vercel.app";
 
 const router = createBrowserRouter([
